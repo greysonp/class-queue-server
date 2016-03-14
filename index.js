7b27ae9f2f23734c0843a46451dbf775a86498ce
Greyson Parrelli <greyson.parrelli@live.com>
2016-03-14 07:57:30 +0000

    Working push notifications.

--- index.js
+++ index.js
@@ -3,27 +3,35 @@
 var express = require('express');
 var app = express();
 var firebase = require('firebase');
+var gcm = require('node-gcm');
 
 var _subIds = [];
+var _gcmSender = new gcm.Sender(process.env.GCM_API_KEY);
 
 function init() {
     getFirebase().child('subscription_ids').on('value', function(snapshot) {
         _subIds = formatSubIdsFromSnapshot(snapshot.val());
-        console.log('Sub Ids', _subIds);
     });
     
     // Listen for new questions, ignore initial batch by filtering out existing questions
     getFirebase().child('questions').orderByChild('time').startAt(Date.now()).on('child_added', function(snapshot) {
         console.log('Child added', snapshot.val());
-        for (var i = 0; i < _subIds.length; i++) {
-            notifySubscriber(_subIds[i], snapshot.val());
-        }
+        notifySubscribers(snapshot.val());
     });
 }
 
-function notifySubscriber(subId, question) {
-    console.log('Notifying subscriber: ' + subId, question);
-    // TODO
+function notifySubscribers(question) {
+    var message = new gcm.Message();
+    message.addNotification({
+        title: 'Computer ' + question.computer_number + ' asked a question.',
+        icon: 'images/icon.png'
+    });
+    _gcmSender.send(message, { registrationTokens: _subIds }, function(err, response) {
+       if (err) {
+           console.error(err);
+       }
+       console.log(response);
+    });
 }
 
 function formatSubIdsFromSnapshot(obj) {
